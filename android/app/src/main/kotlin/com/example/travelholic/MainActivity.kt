4fcbcac8313564e387cde76541d992aeac479525
Fariq Naufal Diaz <fariq.diaz@bankina.id>
2022-06-06 15:14:49 +0700

    Add themes or shared variables

---
+++ android/app/src/main/kotlin/com/example/travelholic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travelholic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Color primary / keseluruhan
+Color primaryColor = Color(0xff5C40CC);
+//Color text / buat teks
+Color blackColor = Color(0xff1F1449);
+//Color header text white / warna putih
+Color whiteColor = Color(0xffFFFFFF);
+//Color desc content / warna silver
+Color silverColor = Color(0xff9698A9);
+//Color boolean condition / warna hijau
+Color greenColor = Color(0xff0EC3AE);
+//Color booelan condition / warna merah
+Color softredColor = Color(0xffEB70A5);
+//Color background
+Color backgroundColor = Color(0xffEB70A5);
+//Color shape grey / warna abu abu
+Color greyColor = Color(0xffDBD7EC);
+
+//Styling text with fonts
+//Black
+TextStyle textBlack = GoogleFonts.poppins(color: blackColor);
+//White
+TextStyle textWhite = GoogleFonts.poppins(color: whiteColor);
+//Grey
+TextStyle textGrey = GoogleFonts.poppins(color: greyColor);
+//Softred
+TextStyle textSoftRed = GoogleFonts.poppins(color: softredColor);
+//Green
+TextStyle textGreen = GoogleFonts.poppins(color: greenColor);
+//Purple
+TextStyle textPurple = GoogleFonts.poppins(color: primaryColor);
+
+//Fontweight size / styling
+//Fontweight light / agak terang
+FontWeight light = FontWeight.w300;
+//Fontweight black / gelap
+FontWeight black = FontWeight.w900;
+//Fontweight regular
+FontWeight regular = FontWeight.w400;
+//Fontweight medium
+FontWeight medium = FontWeight.w500;
+//Fontweight semiBold
+FontWeight semiBold = FontWeight.w600;
+//Fontweight bold
+FontWeight bold = FontWeight.w700;
+//Fontweight extraBold / sangat tebal
+FontWeight extraBold = FontWeight.w800;
